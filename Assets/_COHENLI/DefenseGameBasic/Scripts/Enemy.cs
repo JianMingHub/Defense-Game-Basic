8632c1c09b5a1390ebf9ac12facdbf1096ab22b3
JianLong <long.mach@atech88.com>
2024-08-09 11:08:41 +0700

    Setup Layer ,Player Weapon Script

--- Assets/_COHENLI/DefenseGameBasic/Scripts/Enemy.cs
+++ Assets/_COHENLI/DefenseGameBasic/Scripts/Enemy.cs
@@ -32,8 +32,10 @@ namespace COHENLI.DefenseBasic
         void Update()
         {
             if(IsComponentsNull()) return;
+
+            float distToPlayer = Vector2.Distance(m_player.transform.position, transform.position);
             
-            if(Vector2.Distance(m_player.transform.position, transform.position) <= atkDistance)
+            if(distToPlayer <= atkDistance)
             {
                 m_amin.SetBool(Const.ATTACK_ANIM, true);
                 m_rb.velocity = Vector2.zero;   // (0,0)
@@ -42,6 +44,15 @@ namespace COHENLI.DefenseBasic
             {
                 m_rb.velocity = new Vector2(-speed, m_rb.velocity.y);
             }
+        }
+        public void Die()
+        {
+            Debug.Log("Die");
+            if(IsComponentsNull()) return;
+
+            m_amin.SetTrigger(Const.DEAD_ANIM);
+            m_rb.velocity = Vector2.zero;
+            gameObject.layer = LayerMask.NameToLayer(Const.DEAD_ANIM);
         }
     }
 }
---
+++ Assets/_COHENLI/DefenseGameBasic/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using COHENLI.DefenseBasic;
+using UnityEngine;
+
+namespace COHENLI.DefenseBasic
+{
+    public class Weapon : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if(col.CompareTag(Const.ENEMY_TAG))
+            {
+                Enemy enemy = col.GetComponent<Enemy>();
+                if(enemy)
+                {
+                    enemy.Die();
+                }
+            }
+        }
+    }
+}
+
