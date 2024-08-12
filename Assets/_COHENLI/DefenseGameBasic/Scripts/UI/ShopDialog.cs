a84e21d5257c59e966adc2f446778906bafa09bb
JianLong <long.mach@atech88.com>
2024-08-12 22:43:17 +0700

    Create GUIManager Script

---
+++ Assets/_COHENLI/DefenseGameBasic/Scripts/UI/ShopDialog.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace COHENLI.DefenseBasic
+{
+
+    public class ShopDialog : Dialog
+    {
+        
+    }
+}
